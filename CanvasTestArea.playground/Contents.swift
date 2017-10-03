45487a6b2321df3c0992003663f9f316b955678b
Kanu, Chinedum <ckanu@lcs-mba3120.lcs.on.ca>
2017-10-03 15:20:06 -0400

    Got to clouds.

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -20,20 +20,26 @@
 import Cocoa
 import PlaygroundSupport
 
+
+
 // Create a new canvas
-let canvas = Canvas(width: 300, height: 500)
+let canvas = Canvas(width: 500, height: 500)
 
-// Draw a circle at the origin with radius of 50 pixels
-canvas.drawEllipse(centreX: 0, centreY: 0, width: 50, height: 50)
+// Color in the blue rectangle
+canvas.fillColor = Color.init(hue: 200, saturation: 50, brightness: 100, alpha: 100)
+canvas.drawRectangle(centreX: 0, centreY: 0, width: 1000, height: 1200)
 
-// Show where the origin is
-canvas.drawAxes()
+// draw sun
+canvas.fillColor=Color.yellow
+canvas.drawEllipse(centreX: 0, centreY: 500, width: 200, height: 200)
 
-// Draw an ellipse in a different color at the centre of the canvas
-canvas.fillColor = Color.green
-canvas.drawEllipse(centreX: 150, centreY: 250, width: 50, height: 100)
+// Draw Cloud
+canvas.fillColor=Color.white
+canvas.drawEllipse(centreX: 450, centreY: 500, width: 150, height: 150, borderWidth: 0)
 
-// add a rectangle to the top of the screen
-canvas.drawRectangle(centreX: 150, centreY: 400, width: 100, height: 50)
+canvas.drawEllipse(centreX: 500, centreY: 450, width: 150, height: 150, borderWidth: 0)
+
+canvas.lineColor = Color.orange
+
 // This code is necessary to see the result in the Assistant Editor at right
 PlaygroundPage.current.liveView = canvas.imageView
